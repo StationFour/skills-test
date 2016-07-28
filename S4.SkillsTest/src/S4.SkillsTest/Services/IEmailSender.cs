bb4b05b244fa31e1612406cce906090ed624b535
Christopher Lahey <lahey@stationfour.com>
2016-07-28 15:19:47 -0400

    added skills test to repo

---
+++ S4.SkillsTest/src/S4.SkillsTest/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S4.SkillsTest.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ S4.SkillsTest/src/S4.SkillsTest/ViewModels/Manage/RemoveLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S4.SkillsTest.ViewModels.Manage
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
